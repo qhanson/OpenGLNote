e38fde735796dbd1eaa698db87a0364b92a3b8fc
hanson <yifeihuang17@gmail.com>
2023-04-01 19:31:46 +0800

    Init Project with GLFW, GLAD, GLM, and IMGUI.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+
+
+#include <glm/glm.hpp>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    std::cout<<"Hello OpenGL"<<std::endl;
+    return 0;
+}
